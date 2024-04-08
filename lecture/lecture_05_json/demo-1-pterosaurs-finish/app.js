3e90a275ebcdc2034a6964453bc74e17360cfbd1
Kyle <kmthayer@uw.edu>
2024-04-08 11:36:26 -0700

    do pterosaur demo

--- lecture/lecture_05_json/demo-1-pterosaurs-finish/app.js
+++ lecture/lecture_05_json/demo-1-pterosaurs-finish/app.js
@@ -1,3 +1,4 @@
+import {promises as fs} from 'fs'
 import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
@@ -18,5 +19,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/api/getPterosaurs", async (req, res) => {
+    const data = await fs.readFile("data/pterosaur.json")
+    let pterosaurInfo = JSON.parse(data)
+
+    // filter out any without images
+    let filteredPterosaurInfo =
+        pterosaurInfo.filter(onePterosaur => onePterosaur.img != "") 
+
+    res.json(filteredPterosaurInfo)
+})
 
 export default app;
