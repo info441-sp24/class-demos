02c86876221bfbd87c4738cc2d302060868d1b9a
weifan_certik <weifan.wu@certik.com>
2024-04-10 08:32:57 -0700

    chagne the conenction sttrinh

--- lab/lab3/routes/conection.js
+++ lab/lab3/routes/conection.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 async function connect() {
-    await mongoose.connect("mongodb+srv://weifan:info441@info441.wfotfpj.mongodb.net/");
+    await mongoose.connect("YOUR_MONGODB_CONNECTION_STRING");
 }
 
 module.exports = connect;
