1e954a3f5eac3fa6a58ad19b16dccabb1173005a
Kyle <kmthayer@uw.edu>
2024-04-15 11:40:31 -0700

    add sql code

--- lecture/lecture_07_security_xss_sql/demo-2-sql-finish/routes/users.js
+++ lecture/lecture_07_security_xss_sql/demo-2-sql-finish/routes/users.js
@@ -1,9 +1,59 @@
+import sqlite3 from 'sqlite3'
 import express from 'express';
 var router = express.Router();
 
+
+let db = new sqlite3.Database(':memory:', (err) => {
+  if(err)
+  {
+      return console.error(err.message)
+  }
+  console.log("Connected to the in-memory sqlite database")
+})
+
+db.serialize(() => {
+  db
+      .run('CREATE TABLE people(first_name text, last_name text)')
+      .run(`INSERT INTO people(first_name, last_name)
+            VALUES ("Kyle", "Thayer"),
+                   ("Kyle", "Chandler"),
+                   ("Miranda", "Ma"),
+                   ("Jared", "Lim")
+      
+      `)
+  
+      .run('CREATE TABLE secret_table(message text)')
+      .run(`INSERT INTO secret_table(message)
+            VALUES ('The password for Kyle is: pa55w0rd'),
+                   ('The treasure is hidden on the 5th floor.')
+       
+      `)
+})
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  let nameSearch = req.query.nameSearch
+  nameSearch = nameSearch ? nameSearch : ""
+
+  db.all(`SELECT * FROM people WHERE first_name = "${nameSearch}"`,
+    (err, allRows) => {
+      if(err){
+        console.log("db error: " + err)
+        res.send("db error" + err)
+        return
+      }
+      if(!allRows){
+        return ""
+      }
+      let matchingPeople = allRows.map(
+        row => `${row.first_name} ${row.last_name}`
+      ).join("\n")
+      
+      res.send(matchingPeople)
+    }
+  )
+
 });
 
 export default router;
