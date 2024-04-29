8fe39a2a64dd35919c364c1d73faa7de3d0e15ba
Kyle <kmthayer@uw.edu>
2024-04-29 11:40:05 -0700

    add playlists to playlist demo

--- lecture/lecture_11_user_actions_2/demo-1-playlists-finish/public/javascripts/index.js
+++ lecture/lecture_11_user_actions_2/demo-1-playlists-finish/public/javascripts/index.js
@@ -23,11 +23,23 @@ async function loadUsers(){
             <strong>Favorite Bands:</strong> ${userInfo.favorite_bands.join(", ")}<br>
             <strong>Add Band</strong> <input type="text" id="add_band_text_${userInfo._id}" />
             <button onclick="addBand('${userInfo._id}')">Add Band</button>
+
+            <h3>Playlists</h3>
+            <div id="playlist_div_${userInfo._id}">Loading playlists...</div>
+
+            <h3>Add Playlist</h3>
+            <strong>Title:</strong> <input type="text" id="add_playlist_title_text_${userInfo._id}" /> <br>
+            <strong>Songs:</strong> <input type="text" id="add_playlist_songs_text_${userInfo._id}" /> <br>
+            <button onclick="addPlaylist('${userInfo._id}')">Add Playlist</button>
         </div>        
         `
     }).join("")
 
     document.getElementById("allusersdiv").innerHTML = usersHTML
+
+    usersJson.forEach(userInfo => {
+        loadPlaylistForUser(userInfo._id)
+    })
 }
 
 async function addBand(id){
@@ -41,6 +53,40 @@ async function addBand(id){
             band: bandToAdd
         })
     })
+}
+
+async function addPlaylist(userId){
+    let title = document.getElementById("add_playlist_title_text_" + userId).value
+    let songs = document.getElementById("add_playlist_songs_text_" + userId).value
+
+    await fetch("/api/v1/playlists", {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            title: title,
+            songs: songs,
+            userId: userId
+        })
+    })
+
+}
+
+async function loadPlaylistForUser(userId){
+    // get playlist for the given user
+    let response = await fetch("/api/v1/playlists?userId=" + userId)
+    let playlistJSON = await response.json()
+
+    // add html to the right div for that user
+    let playlistHTML = playlistJSON.map(playlistInfo => {
+        return `
+        <div>
+            <h4>Playlist: ${playlistInfo.title}</h4>
+            <strong>Songs:</strong> ${playlistInfo.songs}
+        </div>
+        `
+    }).join("")
+
+    document.getElementById("playlist_div_"+userId).innerHTML = playlistHTML
 }
 
 // async function loadUsers(){
@@ -79,53 +125,6 @@ async function addBand(id){
 //     })
 // }
 
-// async function loadPlaylistsForUser(userId){
-//     // get playlists for that user
-//     let response = await fetch("/api/v1/playlists?userId=" + userId)
-//     let playlistJSON = await response.json()
-
-//     // add html to the right div for that user
-//     let playlistHTML = playlistJSON.map(playalistInfo => {
-//         return `
-//         <div>
-//             <h4>Playlist: ${playalistInfo.title}</h4>
-//             <strong>Songs:</strong> ${playalistInfo.songs}
-//         </div>
-//         `
-//     }).join("")
-
-//     document.getElementById("playlist_div_" + userId).innerHTML = playlistHTML
-
-// }
-
-// async function addPlaylist(id){
-//     let title = document.getElementById("add_playlist_title_text_" + id).value
-//     let songs = document.getElementById("add_playlist_song_text_" + id).value
-
-//     await fetch("/api/v1/playlists", {
-//         method: "POST",
-//         headers: {'Content-Type': 'application/json'},
-//         body: JSON.stringify({
-//             title: title,
-//             songs: songs,
-//             userId: id
-//         })
-//     })
-
-// }
-
-// async function addBand(id){
-//     let bandToAdd = document.getElementById("add_band_text_" + id).value
-
-//     await fetch("/api/v1/users/bands", {
-//         method: "POST",
-//         headers: {'Content-Type': 'application/json'},
-//         body: JSON.stringify({
-//             userId: id,
-//             band: bandToAdd
-//         })
-//     })
-// }
 
 // async function deleteUser(id){
 //     await fetch("/api/v1/users", {
