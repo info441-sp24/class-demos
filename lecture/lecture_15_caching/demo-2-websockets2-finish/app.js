c70fd8d25a9d56fa4e64197d12830d778a686211
Kyle <kmthayer@uw.edu>
2024-05-13 11:57:15 -0700

    finish socket demo

--- lecture/lecture_15_caching/demo-2-websockets2-finish/app.js
+++ lecture/lecture_15_caching/demo-2-websockets2-finish/app.js
@@ -5,28 +5,47 @@ const app = express()
 enableWs(app)
 
 let socketCounter = 1
-let allSockets = []
+let allSockets = {}
 
 app.ws('/chatSocket', (ws, res) => {
     let mySocketNum = socketCounter
     socketCounter++
     console.log("user " + mySocketNum + " connected via websocket")
 
-    // add this ws to the global array tracking all websockets
-    allSockets.push(ws)
+    // add this ws to the global object tracking all websockets
+    allSockets[mySocketNum] = {
+        socket: ws,
+        name: mySocketNum
+    }
 
-    ws.on('message', chatMsg => {
-        console.log(`msg (user ${mySocketNum}):  ${chatMsg}`)
-        // I want to send the message I got to all the other
-        // browsers connected (using their websockets)
-        allSockets.forEach(socket => {
-            socket.send(`${mySocketNum}: ${chatMsg}`)
-        })
+    ws.on('message', msg => {
+        try{
+            const socketMessage = JSON.parse(msg)
+            if(socketMessage.action == "sendChat"){
+                console.log(`msg (user ${mySocketNum}):  ${socketMessage.chat}`)
+                const myName = allSockets[mySocketNum].name
+                // I want to send the message I got to all the other
+                // browsers connected (using their websockets)
+                for(const [SocketNum, socketInfo] of Object.entries(allSockets)){
+                    socketInfo.socket.send(`${myName}: ${socketMessage.chat}`)
+                }
+            }else if (socketMessage.action == "updateName"){
+                console.log("user " + mySocketNum + " is trying to update their name to " + 
+                    socketMessage.name
+                )
+                allSockets[mySocketNum].name = socketMessage.name
+            }else {
+                console.error("got unexpected action " + socketMessage.action)
+            }
+        }catch(error){
+            console.error("Websocket message received error: " + error)
+        }
+        
     })
 
     ws.on('close', () => {
+        delete allSockets[mySocketNum]
         console.log(`user ${mySocketNum} disconnected`)
-        console.log("I should probably delete them from the array or something")
     })
 
 })
