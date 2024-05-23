25e3daa21cb949d28a16981ad813fa64966fde8d
Kyle <kmthayer@uw.edu>
2024-05-23 14:47:44 -0700

    Put websocket logic in a router file (using a function as a hack to add the websocket to app)

--- lecture/websockets_in_express_starter/express-starter-with-websockets/app.js
+++ lecture/websockets_in_express_starter/express-starter-with-websockets/app.js
@@ -4,7 +4,7 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import enableWs from 'express-ws'
 
-import usersRouter from './routes/users.js';
+import {usersRouter, addUserWebsocket}  from './routes/users.js';
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
@@ -23,31 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', usersRouter);
 
-let socketCounter = 1
-let allSockets = []
+addUserWebsocket(app)
 
-app.ws('/chatSocket', (ws, res) => {
-    let mySocketNum = socketCounter
-    socketCounter++
-    console.log("user " + mySocketNum + " connected via websocket")
-
-    // add this ws to the global array tracking all websockets
-    allSockets.push(ws)
-
-    ws.on('message', chatMsg => {
-        console.log(`msg (user ${mySocketNum}):  ${chatMsg}`)
-        // I want to send the message I got to all the other
-        // browsers connected (using their websockets)
-        allSockets.forEach(socket => {
-            socket.send(`${mySocketNum}: ${chatMsg}`)
-        })
-    })
-
-    ws.on('close', () => {
-        console.log(`user ${mySocketNum} disconnected`)
-        console.log("I should probably delete them from the array or something")
-    })
-
-})
 
 export default app;
--- lecture/websockets_in_express_starter/express-starter-with-websockets/routes/users.js
+++ lecture/websockets_in_express_starter/express-starter-with-websockets/routes/users.js
@@ -6,4 +6,35 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-export default router;
+
+function addUserWebsocket(app){
+  let socketCounter = 1
+  let allSockets = []
+  
+  // NOTE: I have to manually add "/users" here
+  app.ws('/users/chatSocket', (ws, res) => {
+      let mySocketNum = socketCounter
+      socketCounter++
+      console.log("user " + mySocketNum + " connected via websocket")
+  
+      // add this ws to the global array tracking all websockets
+      allSockets.push(ws)
+  
+      ws.on('message', chatMsg => {
+          console.log(`msg (user ${mySocketNum}):  ${chatMsg}`)
+          // I want to send the message I got to all the other
+          // browsers connected (using their websockets)
+          allSockets.forEach(socket => {
+              socket.send(`${mySocketNum}: ${chatMsg}`)
+          })
+      })
+  
+      ws.on('close', () => {
+          console.log(`user ${mySocketNum} disconnected`)
+          console.log("I should probably delete them from the array or something")
+      })
+  
+  })
+}
+
+export {router as usersRouter, addUserWebsocket};
